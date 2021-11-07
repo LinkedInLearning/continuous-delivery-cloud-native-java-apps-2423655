91ba70abe095cd08c1c8e504cb55ffc8df7ce43f
kmb385 <kmb385@gmail.com>
2021-11-07 20:05:20 +0000

    Added dependency

---
+++ playground/src/main/java/com/linkedin/javacd/Application.java
@@ -0,0 +1,10 @@
+package com.linkedin.javacd;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println(StringUtils.capitalize("hello learner"));
+	}
+}
